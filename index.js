a11a313dc1ebc9d30695420386313fa90f56f547
Koolghost69 <39455569+koolghost69@users.noreply.github.com>
2021-08-09 18:05:38 +1000

    Update index.js

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ var int = Math.floor(Math.random() * 99999999999999)
 var start = int
 var input = 1
 var now = int % 2
-var winning = Math.floor(Math.random() * 500000)
+var winning = Math.floor(Math.random() * 500)
 var name = os.hostname()
 var int = 0
 var times = 0
 var rl = require('readline')
+const { Database } = require("quickmongo");
+const mongo = new Database('mongodb+srv://DatabaseAccessBot:UxrGv6RkmCq26Ndk@cluster0.jxuwi.mongodb.net/chronoCoin')
+const talkedRecently = new Set();
 const { Webhook } = require('discord-webhook-node');
-const hook = new Webhook("https://discord.com/api/webhooks/873488324759810078/sfj9Mk70v6T19AnCYfrCmY3wpMTudnGJdoQhw1QvutjaC9seBfxSX_dsohp8Dy7LlfUR");
+const hook = new Webhook('https://discord.com/api/webhooks/873764541425680445/Sfam4FQ52JHEIWC4Da__wKKarR01VjGzNaTZGxYA6XLbhDjujMhppCoGh0n4me4f60DA');
 
 const IMAGE_URL = 'https://cdn.discordapp.com/attachments/798714516275068940/873514924444356608/gigachad.png';
 hook.setUsername('eCoin');
 hook.setAvatar(IMAGE_URL);
 
 var prompts = rl.createInterface(process.stdin, process.stdout)
+prompts.question('What is your discord user ID? ', function(answer) {
+(async () => {
+const account = await mongo.fetch(`${answer}.key`) 
 
-    var times = 0
+
+if(account) {   
+    var prompts = rl.createInterface(process.stdin, process.stdout)
+    prompts.question('What is your private key? ', function(key) {
+        if(key === account) {
+
+    (async () => { 
+        var times = 0   
     while (true) {
-
-        var int = Math.floor(Math.random() * 500000)
-        sleep.msleep(100)
-    
-        console.log(int + `| ${winning}`)
-        if(int === winning) {
-            return hook.send(`WE have a winner there pc name is : ${name} (coin 1/5)`);
-        }
-    }    
-
-
-
-
